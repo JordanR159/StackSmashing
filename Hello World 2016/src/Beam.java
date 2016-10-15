fb60ec45ab0ccbbc29814bac9a30504460180ae9
JordanR159 <reed226@purdue.edu>
2016-10-15 19:51:15 -0400

    Beam Class added

--- Hello World 2016/src/Beam.java
+++ Hello World 2016/src/Beam.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
 
-public class Beam {
+public class Beam 
+{
+	private double startX;
+	private double startY;
+	private boolean toRight;
+	
+	public boolean useBeam(Player att,Player def,ArrayList<Block> obstacles)
+	{
+		Beam shot = new Beam(att.getPosX(),att.getPosY(),att.getVelocityX());
+		return isOnTarget(def,obstacles);
+		
+	}
+	
+	public Beam(double x,double y,double vx)
+	{
+		startX = x;
+		startY = y;
+		if(vx > 0)
+			toRight = true;
+		else if(vx < 0)
+			toRight = false;
+		else if(x > 802)
+			toRight = false;
+		else
+			toRight = true;
+		
+
+	}
+	
+	public boolean isOnTarget(Player def, ArrayList<Block> obstacles)
+	{
+		return false;
+	}
+	
 
 }
