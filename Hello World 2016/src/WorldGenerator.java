132e98f22ec3c7c0e851010f634e6039ec8d8cbf
JordanR159 <reed226@purdue.edu>
2016-10-15 17:29:43 -0400

    aw shit

--- Hello World 2016/src/WorldGenerator.java
+++ Hello World 2016/src/WorldGenerator.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
 
 public class WorldGenerator 
 {
+	public ArrayList<Block> generateBlocks(int numBlocks)
+	{
+		ArrayList<Block> blocks = new ArrayList<Block>();
+		double x, y, wid, hei;
+		boolean overlap = true;
+		x = 0;
+		y = 0;
+		for(int i = 0; i < numBlocks; i++)
+		{
+			while(overlap)
+			{
+				x = Math.random()*1904;
+				y = Math.random()*952;
+				overlap = false;
+				for(int j = 0; j < blocks.size(); j++)
+				{
+					Block check = blocks.get(j);
+					if(x > check.getPosX() && x < check.getPosX()+check.getWidth() 
+					&& y > check.getPosY() && y < check.getPosY()+check.getHeight())
+						overlap = true;
+				}
+
+			}
+			wid = Math.random()*50;
+			hei = Math.random()*50;
+			Block curr = new Block(x,y,wid,hei);
+			blocks.add(curr);
+			overlap = true;
+		}		
+		return blocks;
+	}
 
 }
