ff480681ae5d6d97229b977f3b340a289994283c
JordanR159 <reed226@purdue.edu>
2016-10-15 18:30:45 -0400

    h

--- Hello World 2016/src/WorldGenerator.java
+++ Hello World 2016/src/WorldGenerator.java
@@ -11,22 +11,36 @@ public class WorldGenerator
 		y = 0;
 		for(int i = 0; i < numBlocks; i++)
 		{
+			wid = Math.random();
+			hei = Math.random();
+			if(wid < 0.333)
+				wid = 50;
+			else if(wid > 0.666)
+				wid = 150;
+			else
+				wid = 100;
+			if(hei < 0.555)
+				hei = 50;
+			else if(wid > 0.888)
+				hei = 150;
+			else
+				hei = 100;
 			while(overlap)
 			{
-				x = Math.random()*1904;
-				y = Math.random()*952;
+				x = Math.random()*1654+50;
+				y = Math.random()*521+150;
 				overlap = false;
 				for(int j = 0; j < blocks.size(); j++)
 				{
 					Block check = blocks.get(j);
-					if(x > check.getPosX() && x < check.getPosX()+check.getWidth() 
-					&& y > check.getPosY() && y < check.getPosY()+check.getHeight())
+					if((x >= check.getPosX() && x <= check.getPosX()+check.getWidth()) 
+					|| (x+wid >= check.getPosX() && x+wid <= check.getPosX()+check.getWidth())
+					&& (y >= check.getPosY() && y <= check.getPosY()+check.getHeight())
+					|| (y+hei >= check.getPosY() && y+hei <= check.getPosY()+check.getHeight()))
 						overlap = true;
 				}
 
 			}
-			wid = Math.random()*50;
-			hei = Math.random()*50;
 			Block curr = new Block(x,y,wid,hei);
 			blocks.add(curr);
 			overlap = true;
