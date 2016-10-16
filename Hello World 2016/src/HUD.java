2fa77d17e7ec91f1ab2fdc415cc826a87e680387
Killian-LeClainche <kleclain@purdue.edu>
2016-10-16 04:45:10 -0500

    stuff

--- Hello World 2016/src/HUD.java
+++ Hello World 2016/src/HUD.java
@@ -1,5 +1,4 @@
 import java.text.DecimalFormat;
-import java.util.Random;
 
 import org.lwjgl.opengl.GL11;
 
@@ -38,29 +37,41 @@ public class HUD extends GuiScreen
 		Draw.rect(0, 1016, 1920, 1080, 1);
 		GL11.glEnd();
 		
-		Player player1 = world.getPlayers().get(0);
-		Player player2 = world.getPlayers().get(1);
+		double player1 = world.getPlayers().get(0).getSize() - 25;
+		double player2 = world.getPlayers().get(1).getSize() - 25;
 		
 		double breakerX = (defaultFont.getTextWidth(" :  ", 64)) / 2 - 12.5;
-		double rightX = defaultFont.getTextWidth(sizeFormat.format(100), 64);
+		double rightX = defaultFont.getTextWidth(sizeFormat.format(Math.max(player1 / .75, 0)), 64);
+		
 		OpenGL.glColor(1d, 1d, 1d, 1d);
+		
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		
 		Texture.glBindTexture("font:basic");
+		
 		defaultFont.drawString("" + (int)Math.round(1 / delta / 10) * 10, 10, 10, 10, 64);
 		defaultFont.drawUpString(" :  ", 1920 / 2 - breakerX, 1067, 20, 48);
+		
 		OpenGL.glColor(renderer.getPlayer(0));
-		defaultFont.drawUpString(sizeFormat.format((player1.getSize() - 25) / .75), 1920 / 2 - breakerX - rightX, 1071, 20, 64);
+		defaultFont.drawUpString(sizeFormat.format(Math.max(player1 / .75, 0)), 1920 / 2 - breakerX - rightX, 1071, 20, 64);
+		
 		OpenGL.glColor(renderer.getPlayer(1));
-		defaultFont.drawUpString(sizeFormat.format((player2.getSize() - 25) / .75), (1920 + breakerX) / 2, 1071, 20, 64);
+		defaultFont.drawUpString(sizeFormat.format(Math.max(player2 / .75, 0)), (1920 + breakerX) / 2, 1071, 20, 64);
+		
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		
 		OpenGL.glBegin();
+		
 		Color4d color = renderer.getPlayer(0);
+		
 		OpenGL.glColor(color.getRed(), color.getGreen(), color.getBlue(), .5);
-		Draw.rect(8, 1020, 8 + (1920 / 2 - 8) * (player1.getSize() - 25) / 75d, 1074, 19);
+		Draw.rect(8, 1020, 8 + (1920 / 2 - 8) * player1 / 75d, 1074, 19);
+		
 		color = renderer.getPlayer(1);
+		
 		OpenGL.glColor(color.getRed(), color.getGreen(), color.getBlue(), .5);
-		Draw.rect(1920 / 2 + (1920 / 2 - 8) * (player2.getSize() - 25) / 75d, 1020, 1912, 1074, 19);
+		Draw.rect(1920 - (1920 / 2 - 8) * player2 / 75d, 1020, 1912, 1074, 19);
+		
 		GL11.glEnd();
 	}
 
