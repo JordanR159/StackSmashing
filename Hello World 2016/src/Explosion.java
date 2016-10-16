4ca9eb4b6c955bd11bc4007e3bf4ac87fe66113d
Jordan Buckmaster <buckmast@purdue.edu>
2016-10-15 21:53:40 -0400

    explosions

--- Hello World 2016/src/Explosion.java
+++ Hello World 2016/src/Explosion.java
@@ -1,19 +1,31 @@
+import java.util.ArrayList;
 
 public class Explosion
 {
 	private double xPos;
 	private double yPos;
 	private double size;
+	private double midX;
+	private double midY;
 	
 	public Explosion(Player user) {
 		xPos = user.getPosX() - (2 * user.getSize());
 		yPos = user.getPosY() - (2 * user.getSize());
 		size = 5 * user.getSize();
-		
+		midX = user.getPosX() + user.getSize()/2;
+		midY = user.getPosY() + user.getSize()/2;
 	}
 	
-	public boolean isOnTarget(Player targ) {
+	public double calcDamage(Player targ, ArrayList<Block> obstacles) {
+		int amountExposed = 0;
+		//bottom right corner
+		int bRightSlope = 
 		
+		/*if(newPos < check.getPosY() + check.getHeight() && 
+				newPos + curr.getSize() > check.getPosY() && 
+				curr.getPosX() < check.getPosX()+check.getWidth() && 
+				curr.getPosX() + curr.getSize() > check.getPosX()){
+					curr.setPosY(check.getPosY() + check.getHeight());*/
 		
 		return true;
 	}
