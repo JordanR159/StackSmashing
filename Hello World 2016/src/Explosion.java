e5d4b6b402a1d1e04995e65080929c1332de6373
Jordan Buckmaster <buckmast@purdue.edu>
2016-10-15 22:14:17 -0400

    stuff

--- Hello World 2016/src/Explosion.java
+++ Hello World 2016/src/Explosion.java
@@ -18,14 +18,21 @@ public class Explosion
 	
 	public double calcDamage(Player targ, ArrayList<Block> obstacles) {
 		int amountExposed = 0;
-		//bottom right corner
-		int bRightSlope = 0;
+		double bRightSlope = ((targ.getPosY() + targ.getSize()) - midY) /
+							 ((targ.getPosX() + targ.getSize()) - midX);
+		double bLeftSlope = ((targ.getPosY() + targ.getSize()) - midY) /
+							(targ.getPosX() - midX);
+		double tRightSlope = ((targ.getPosY() - midY) /
+				 			 ((targ.getPosX() + targ.getSize()) - midX));
+		double tLeftSlope = (targ.getPosY() - midY) /
+							(targ.getPosX() - midX);
+		
 		
 		/*if(newPos < check.getPosY() + check.getHeight() && 
 				newPos + curr.getSize() > check.getPosY() && 
 				curr.getPosX() < check.getPosX()+check.getWidth() && 
 				curr.getPosX() + curr.getSize() > check.getPosX()){
-					curr.setPosY(check.getPosY() + check.getHeight());s*/
+					curr.setPosY(check.getPosY() + check.getHeight());*/
 		
 		return 0;
 	}
