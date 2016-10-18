46d6c2ef4775dd4f7743e4af9b9d6edeb8e922ce
Killian-LeClainche <kleclain@purdue.edu>
2016-10-18 14:28:06 -0500

    Tweaking Main Menu Stack Smashing Animation

--- Hello World 2016/src/GuiMainMenu.java
+++ Hello World 2016/src/GuiMainMenu.java
@@ -14,10 +14,14 @@ import com.polaris.engine.util.MathHelper;
 public class GuiMainMenu extends GuiScreen
 {
 	
+	private static final double TIMER = 60;
+	
 	private double ticksExisted = 0;
+	
 	private char[] sequence = "STACK SMASHING".toCharArray();
 	private double[] ticks = new double[sequence.length];
 	private double[] toTicks = new double[sequence.length];
+	
 	private FontMap fontMap;
 
 	public GuiMainMenu(Application app) 
@@ -36,15 +40,19 @@ public class GuiMainMenu extends GuiScreen
 		Texture.glBindTexture("font:basic");
 		
 		double x = 1920 / 2 - fontMap.getTextWidth(new String(sequence), 128) / 2;
-		int orange = 0;
+		double orange = 0;
 		
+		Color4d color = new Color4d(1d, 1d, 1d, 1d);
+		double yPos;
 		for(int i = 0; i < sequence.length; i++)
 		{
-			fontMap.drawColorString("" + sequence[i], x, 200 + ticks[i], 0, 128, new Color4d(255, orange += 200 / sequence.length, 0, (int) Math.min(ticksExisted * 60, 255)));
+			yPos = 200 + (ticksExisted * TIMER > 255 ? Math.min(Math.pow((ticksExisted * TIMER - 255), 1.5), 200) : 0) + ticks[i];
+			color.setColor(1d, orange += 200 / 255d / sequence.length, 0, Math.min(ticksExisted * TIMER / 255d, 1d));
+			fontMap.drawColorString("" + sequence[i], x, yPos, 0, 128, color);
 			x += fontMap.getTextWidth("" + sequence[i], 128);
 		}
 		
-		if(ticksExisted * 60 > 265)
+		if(ticksExisted * TIMER > 265)
 		{
 			if(App.getMouseX() >= 1920 / 2 - fontMap.getTextWidth("START A GAME", 48) && App.getMouseX() <= 1920 / 2 + fontMap.getTextWidth("START A GAME", 48))
 			{
@@ -58,17 +66,17 @@ public class GuiMainMenu extends GuiScreen
 		
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 		
-		if(ticksExisted * 60 > 255)
+		if(ticksExisted * TIMER > 255)
 		{
 			for(int i = 0; i < ticks.length; i++)
 			{
 				if(MathHelper.isEqual(ticks[i], toTicks[i]))
 				{
-					toTicks[i] = 200 + Math.random() * 20 - 10;
+					toTicks[i] = Math.random() * 20 - 10;
 				}
 				else
 				{
-					ticks[i] = MathHelper.getExpValue(ticks[i], toTicks[i], 1, delta);
+					ticks[i] = MathHelper.getExpValue(ticks[i], toTicks[i], .1, delta);
 				}
 			}
 		}
@@ -81,8 +89,8 @@ public class GuiMainMenu extends GuiScreen
 	
 	public boolean mouseClick(int mouseId)
 	{
-		if(ticksExisted * 60 < 255)
-			ticksExisted = 255 / 60d;
+		if(ticksExisted * TIMER < 255)
+			ticksExisted = 255 / TIMER;
 		if(App.getMouseX() >= 1920 / 2 - fontMap.getTextWidth("START A GAME", 48) && App.getMouseX() <= 1920 / 2 + fontMap.getTextWidth("START A GAME", 48))
 		{
 			if(App.getMouseY() >= 600 - 32 && App.getMouseY() <= 600 + 32)
